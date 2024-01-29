2f4649362e942862fe080afa6c4f728445e10798
yuri19951117 <154530214+yuri19951117@users.noreply.github.com>
2024-01-29 16:17:50 +0900

    kauai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+const addBtn = document.getElementById('text');
+addBtn.innerHTML = 'ボタンをクリックしました'
+ });
+
